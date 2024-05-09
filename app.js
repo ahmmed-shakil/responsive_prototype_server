dec4f0cd8fe40c1e397af703987eb7230e0b3d19
ahmmed-shakil <shakilahmmed252@gmail.com>
2024-05-09 15:57:34 +0600

    Cors options added

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ const cors = require("cors");
 const messageRouter = require("./routes/route");
 
 // midlewares
+const corsOptions = {
+  origin: "https://responsive-prototype.onrender.com", // Allow specific origin
+  methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
+  allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+};
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
   res.send("Route is working!");
